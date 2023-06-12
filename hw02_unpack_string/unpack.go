43eb8c84eea4bb377a51a930397ab36029e4be00
efedyakov <e.fedyakov@gmail.com>
2023-06-12 16:29:43 +0500

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -14,21 +14,30 @@ func Unpack(str string) (string, error) {
 	var bbr rune = 0
 	for _, r := range str {
 
-		if bbr != '\\' && br == '\\' && (r == '\\' || unicode.IsDigit(r)) {
-
-		} else if bbr == '\\' && br == '\\' && r == '\\' {
-			b.WriteRune(br)
-			br = 0
-		} else if unicode.IsDigit(r) {
-			if br == 0 {
-				return "", ErrInvalidString
+		switch {
+		case bbr != '\\' && br == '\\' && (r == '\\' || unicode.IsDigit(r)):
+			{
 			}
-			for i := 0; i < int(r)-'0'; i++ {
+		case bbr == '\\' && br == '\\' && r == '\\':
+			{
+				b.WriteRune(br)
+				br = 0
+			}
+		case unicode.IsDigit(r):
+			{
+				if br == 0 {
+					return "", ErrInvalidString
+				}
+				for i := 0; i < int(r)-'0'; i++ {
+					b.WriteRune(br)
+				}
+				r = 0
+			}
+		case br != 0:
+			{
 				b.WriteRune(br)
 			}
-			r = 0
-		} else if br != 0 {
-			b.WriteRune(br)
+
 		}
 		bbr = br
 		br = r
