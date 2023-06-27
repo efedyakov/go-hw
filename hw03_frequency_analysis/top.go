6c3e4efc8580bbd0d78f8df10b59b152d62b6f9d
efedyakov <e.fedyakov@gmail.com>
2023-06-27 14:05:42 +0500

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -17,9 +17,11 @@ func split(input string) map[string]int {
 }
 
 func sortMapByValue(intput map[string]int) []string {
-	var keys []string
+	var keys []string = make([]string, len(intput))
+	index := 0
 	for key := range intput {
-		keys = append(keys, key)
+		keys[index] = key
+		index++
 	}
 	sort.Slice(keys, func(i, j int) bool {
 		return intput[keys[i]] > intput[keys[j]] || (intput[keys[i]] == intput[keys[j]] && keys[i] < keys[j])
