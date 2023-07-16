2850e0b231c1c46b33aeba9367d21454704ed7a9
efedyakov <e.fedyakov@gmail.com>
2023-07-16 22:34:41 +0500

    HW4 is completed

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,92 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	Root ListItem
+	len  int
+}
+
+func (l *list) Len() int { return l.len }
+
+func (l *list) Front() *ListItem {
+	if l.len == 0 {
+		return nil
+	}
+	return l.Root.Next
+}
+
+func (l *list) Back() *ListItem {
+	if l.len == 0 {
+		return nil
+	}
+	return l.Root.Prev
+}
+
+func (l *list) insert(e, at *ListItem) *ListItem {
+	l.Root.Next.Prev = &l.Root
+	l.Root.Prev.Next = &l.Root
+	e.Prev = at
+	e.Next = at.Next
+	e.Prev.Next = e
+	e.Next.Prev = e
+	l.Root.Next.Prev = nil
+	l.Root.Prev.Next = nil
+
+	l.len++
+	return e
+}
+
+func (l *list) insertValue(v interface{}, at *ListItem) *ListItem {
+	return l.insert(&ListItem{Value: v}, at)
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	return l.insertValue(v, &l.Root)
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	return l.insertValue(v, l.Root.Prev)
+}
+
+func (l *list) Remove(e *ListItem) {
+	l.Root.Next.Prev = &l.Root
+	l.Root.Prev.Next = &l.Root
+	e.Prev.Next = e.Next
+	e.Next.Prev = e.Prev
+	e.Next = nil
+	e.Prev = nil
+	l.Root.Next.Prev = nil
+	l.Root.Prev.Next = nil
+	l.len--
+}
+
+func (l *list) move(e, at *ListItem) {
+	if e == at {
+		return
+	}
+
+	l.Root.Next.Prev = &l.Root
+	l.Root.Prev.Next = &l.Root
+	e.Prev.Next = e.Next
+	e.Next.Prev = e.Prev
+
+	e.Prev = at
+	e.Next = at.Next
+	e.Prev.Next = e
+	e.Next.Prev = e
+	l.Root.Next.Prev = nil
+	l.Root.Prev.Next = nil
+}
+
+func (l *list) MoveToFront(e *ListItem) {
+	if l.Root.Next == e {
+		return
+	}
+	l.move(e, &l.Root)
 }
 
 func NewList() List {
-	return new(list)
+	l := new(list)
+	l.Root.Prev = &l.Root
+	l.Root.Next = &l.Root
+	return l
 }
