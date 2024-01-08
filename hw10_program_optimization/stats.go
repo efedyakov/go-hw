b05c04af6e66c37346a91c1f3c18d2582fede480
efedyakov <e.fedyakov@gmail.com>
2024-01-08 19:42:10 +0500

    hw10_program_optimization

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,66 +1,49 @@
 package hw10programoptimization
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
-	"regexp"
 	"strings"
 )
 
 type User struct {
-	ID       int
-	Name     string
-	Username string
-	Email    string
-	Phone    string
-	Password string
-	Address  string
+	Email string
 }
 
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
+	result := make(DomainStat)
+	err := countDomains2(r, domain, &result)
 	if err != nil {
 		return nil, fmt.Errorf("get users error: %w", err)
 	}
-	return countDomains(u, domain)
-}
-
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
-		}
-		result[i] = user
-	}
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
-	result := make(DomainStat)
-
-	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
-		if err != nil {
-			return nil, err
-		}
-
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
-		}
-	}
 	return result, nil
+}
+
+func countDomains2(r io.Reader, domain string, domainStats *DomainStat) error {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		var user User
+		if err := json.Unmarshal(scanner.Bytes(), &user); err != nil {
+			return err
+		}
+		if isInDomains(user, domain) {
+			num := (*domainStats)[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
+			num++
+			(*domainStats)[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		}
+	}
+	return nil
+}
+
+func isInDomains(u User, domain string) bool {
+	lendomain := len(domain)
+	lenemail := len(u.Email)
+	if lendomain >= lenemail {
+		return false
+	}
+	return u.Email[lenemail-lendomain-1] == '.' && u.Email[lenemail-lendomain:] == domain
 }
